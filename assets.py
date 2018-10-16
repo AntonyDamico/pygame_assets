2f50e9a1c13cd312a8d565e1990b0be37bdbba4d
unknown <antonydamico29@gmail.com>
2018-10-16 16:31:51 -0400

    agregando objetos de texto y cambiando nombres de variables

--- assets.py
+++ assets.py
@@ -1,4 +1,11 @@
+# pylint: disable=E1101
+# pylint: disable=E1121
 import pygame
+import colors
+
+def objetos_texto(text, font, text_color=colors.NEGRO):
+    textSurface = font.render(text, True, text_color)
+    return textSurface, textSurface.get_rect()
 
 def dibujarCuadrado(x, y, w, h, gameDisplay, color=None):
     s = pygame.Surface((w, h))
@@ -27,11 +34,11 @@ def boton(x, y, w, h, inactive_color, active_color,  msg='', font_size=20, actio
     # Dice si el boton ha sido presionado o no
     click = pygame.mouse.get_pressed()
 
-    dibujarCuadrado(x - 3, y - 3, w + 6, h + 6, negro)
+    # dibujarCuadrado(x - 3, y - 3, w + 6, h + 6, negro)
      
     if x+w > mouse[0] > x and y + h > mouse[1] > y:
         # Si el cursor está encima del botón, cambia a su color activo (ac)
-        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        pygame.draw.rect(gameDisplay, active_color, (x, y, w, h))
         # Comprueba si el botón fue presionado y posee alguna opción
         if click[0] == 1 and action != None:
             if parameters:
@@ -40,11 +47,11 @@ def boton(x, y, w, h, inactive_color, active_color,  msg='', font_size=20, actio
                 action()
     else:
         # Dibuja el botón con su color inactivo
-        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+        pygame.draw.rect(gameDisplay, inactive_color, (x, y, w, h))
 
     # Fuente y tamano del texto del botón
-    textButton = pygame.font.Font('freesansbold.ttf', fuenteTam)
-    TextSurf, TextRect = text_objects(msg, textButton)
+    textButton = pygame.font.Font('freesansbold.ttf', font_size)
+    TextSurf, TextRect = objetos_texto(msg, textButton)
     # Posicionando el botón
     TextRect.center = ((x+(w/2)), (y+(h/2)))
     # Poniendo el botón en pantalla
